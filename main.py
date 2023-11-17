1401d956876e188473ddea005c5ed776094f0fa5
SanjayKH-Git <sanjayhegde92@gmail.com>
2023-11-17 21:11:59 +0530

    csv json download

--- main.py
+++ main.py
@@ -6,17 +6,31 @@ st.header("YP Tech7c Tool")
 st.write("Enter the Searching Data")
 
 # Country dropdown options
-countries = ["Austrailia", "USA", "Canada", "New Zealand"]
+countries = ["Australia", "USA", "Canada", "New Zealand"]
 
 # Form inputs
 country = st.selectbox("Select Country", countries)
 name = st.text_input("Search Name")
 city = st.text_input("City")
+# st.write("OR")
+# Center and change font color of "OR"
+st.markdown(
+    "<h3 style='text-align:center; color:blue;'>OR</h3>",
+    unsafe_allow_html=True,
+)
+direct_url = st.text_input("Direct URL")
 
 # Submit button
 if st.button("Collect Data"):
     # Validation check
-    if country and name and city:
-        st.write(f"Collected Data for : {name}, {city}, {country}")
+    if country and name and city or direct_url:
+        if direct_url:
+            # st.write(f"Collecting Data for : {name}, {city}, {country}")
+            yp_au_scrape(name, city, direct_url)
+            
+        else:
+            st.write(f"Collecting Data for : {name}, {city}, {country}")
+            yp_au_scrape(name, city)
+
     else:
         st.warning("Please fill in all the inputs before collecting data.")
--- yp_scraper.py
+++ yp_scraper.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import time
 import csv
 import requests
@@ -8,70 +9,142 @@ from tqdm import tqdm
 import json
 import os
 import re
+import pandas as pd
+import sqlite3
+from io import BytesIO
 
 
-def yp_au_scrape(clue, loc_clue, country):
-    clue = "chirofactor"
-    loc_clue = "Brisbane"
-    output_file = "/content/drive/MyDrive/Upwork_data/YP AU/yp_au.csv"
-    headers = [
-        "YP URL",
-        "Business Name",
-        "Website URL",
-        "Phone Number",
-        "Physical Address",
-        "Email",
-    ]
-    with open(output_file, "w", newline="", encoding="utf-8") as output_csv:
-        csvwriter = csv.writer(output_csv)
-        csvwriter.writerow(headers)
+def yp_au_scrape(clue="", loc_clue="", direct_url=""):
+    try:
+        # clue = "chirofactor"
+        # loc_clue = "Brisbane"
+        output_file = "Output_Files/yp_au.csv"
 
-    main_url = f"https://www.yellowpages.com.au/search/listings?clue={clue}&locationClue={loc_clue}"
-    main_resp = requests.get(main_url, headers={"User-Agent": "Mozilla/5.0"})
-    main_soup = Bs(main_resp.text, "html.parser")
-    dom = etree.HTML(str(main_soup))
+        if direct_url:
+            main_url = direct_url
+        else:
+            main_url = f"https://www.yellowpages.com.au/search/listings?clue={clue}&locationClue={loc_clue}"
 
-    result_list = []
-    for card in main_soup.find_all(
-        "div",
-        class_="Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded",
-    ):
-        card_dom = etree.HTML(str(card))
-        title = "".join(
-            card_dom.xpath(
-                "//div[@class='Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded']//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']//h3/text()"
-            )
-        )
-        phone_No = "".join(
-            card_dom.xpath(
-                "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-text ButtonPhone MuiButton-textPrimary MuiButton-fullWidth']//span/text()"
-            )
-        )
-        website = "".join(
-            card_dom.xpath(
-                "//a[@class='MuiButtonBase-root MuiButton-root MuiButton-text ButtonWebsite MuiButton-textSecondary MuiButton-fullWidth']/@href"
-            )
-        )
-        yp_url = "https://www.yellowpages.com.au" + "".join(
-            card_dom.xpath(
-                "//div[@class='Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded']//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']/@href"
-            )
-        )
-        yp_resp = requests.get(yp_url, headers={"User-Agent": "Mozilla/5.0"})
-        yp_soup = Bs(yp_resp.text, "html.parser")
-        yp_dom = etree.HTML(str(yp_soup))
-        email = yp_soup.find("a", class_="contact contact-main contact-email")[
-            "data-email"
-        ]
-        address = "".join(
-            yp_dom.xpath(
-                "//div[@class='listing-address mappable-address mappable-address-with-poi']/text()"
-            )
-        )
-        result = [yp_url, title, website, phone_No, address, email]
-        print(result)
-        result_list.append(result)
+        st.write(f"Searching URL: {main_url}")
+        main_resp = requests.get(main_url, headers={"User-Agent": "Mozilla/5.0"})
+        main_soup = Bs(main_resp.text, "html.parser")
+        dom = etree.HTML(str(main_soup))
 
-    with open(output_file, "a", newline="", encoding="utf-8") as output_csv:
-        csvwriter = csv.writer(output_csv)
-        csvwriter.writerows(result_list)
+        card_list = main_soup.find_all(
+            "div",
+            class_="Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded",
+        )
+        st.write(len(card_list))
+
+        All_result_dict = {}
+        for cnt, card in enumerate(card_list):
+            try:
+                card_dom = etree.HTML(str(card))
+                title = "".join(
+                    card_dom.xpath(
+                        "//div[@class='Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded']//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']//h3/text()"
+                    )
+                )
+                phone_No = "".join(
+                    card_dom.xpath(
+                        "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-text ButtonPhone MuiButton-textPrimary MuiButton-fullWidth']//span/text()"
+                    )
+                )
+                website = "".join(
+                    card_dom.xpath(
+                        "//a[@class='MuiButtonBase-root MuiButton-root MuiButton-text ButtonWebsite MuiButton-textSecondary MuiButton-fullWidth']/@href"
+                    )
+                )
+                yp_url = "https://www.yellowpages.com.au" + "".join(
+                    card_dom.xpath(
+                        "//div[@class='Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded']//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']/@href"
+                    )
+                )
+                yp_resp = requests.get(yp_url, headers={"User-Agent": "Mozilla/5.0"})
+                yp_soup = Bs(yp_resp.text, "html.parser")
+                yp_dom = etree.HTML(str(yp_soup))
+                email = "".join(
+                    yp_dom.xpath(
+                        "//a[@class='contact contact-main contact-email']/@data-email"
+                    )
+                )
+
+                if not email:
+                    Google_Search_url = (
+                        f"https://www.google.com/search?q={title}+email+address"
+                    )
+                    search_resp = requests.get(
+                        Google_Search_url, headers={"User-Agent": "Mozilla/5.0"}
+                    )
+                    email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
+                    email = ", ".join(re.findall(email_pattern, search_resp.text))
+
+                address = "".join(
+                    yp_dom.xpath(
+                        "//div[@class='listing-address mappable-address mappable-address-with-poi']/text()"
+                    )
+                )
+
+                result_dict = {
+                    "YP URL": yp_url,
+                    "Business Name": title,
+                    "Website URL": website,
+                    "Phone Number": phone_No,
+                    "Physical Address": address,
+                    "Email": email,
+                }
+                All_result_dict[result_dict["Business Name"]] = result_dict
+
+                headers = result_dict.keys()
+
+                df = pd.DataFrame([result_dict.values()], columns=headers)
+
+                if cnt == 0:
+                    st_df = st.dataframe(df)
+                else:
+                    st_df.add_rows(df)
+
+            except Exception as e:
+                print(e)
+                pass
+
+        result_df = pd.DataFrame(All_result_dict.values())
+        json_format = json.dumps(All_result_dict)
+        print(All_result_dict)
+
+        csv_col, json_col = st.columns(2)
+
+        if csv_col.download_button(
+            "Download CSV",
+            result_df.to_csv(index=False).encode("utf-8"),
+            file_name="output.csv",
+        ):
+            st.success("CSV file downloaded successfully!")
+
+        if json_col.download_button(
+            "Download JSON",
+            json_format.encode("utf-8"),
+            file_name="output.json",
+        ):
+            st.success("JSON file downloaded successfully!")
+
+        # Create a temporary file for SQLite database
+        # with tempfile.NamedTemporaryFile(suffix=".db", delete=False) as temp_file:
+        #     temp_file_name = temp_file.name
+
+        #     # Convert DataFrame to SQLite database
+        #     with sqlite3.connect(temp_file_name) as conn:
+        #         result_df.to_sql(
+        #             "your_table_name", conn, index=False, if_exists="replace"
+        #         )
+
+        # # Create download button
+        # if st.button("Download SQLite"):
+        #     st.download_button(
+        #         label="Download SQLite", data=temp_file_name, key="download_sqlite"
+        #     )
+        #     st.success("SQLite database file downloaded successfully!")
+
+    except Exception as e:
+        st.write("Can't Find Data")
+        print(yp_url, e)
