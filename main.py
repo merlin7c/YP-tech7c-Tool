ae89359de32bb9401e34428571ec1f907aa483be
SanjayKH-Git <sanjayhegde92@gmail.com>
2023-12-06 19:22:26 +0530

    Major Update

--- main.py
+++ main.py
@@ -1,36 +1,77 @@
+import json
 import streamlit as st
 from yp_scraper import yp_au_scrape
+import os
+import sqlite3
+import pandas as pd
 
-st.header("YP Tech7c Tool")
 
-st.write("Enter the Searching Data")
+if __name__ == "__main__":
+    st.header("YP Tech7c Tool")
 
-# Country dropdown options
-countries = ["Australia", "USA", "Canada", "New Zealand"]
+    st.write("Enter the Searching Data")
 
-# Form inputs
-country = st.selectbox("Select Country", countries)
-name = st.text_input("Search Name")
-city = st.text_input("City")
-# st.write("OR")
-# Center and change font color of "OR"
-st.markdown(
-    "<h3 style='text-align:center; color:blue;'>OR</h3>",
-    unsafe_allow_html=True,
-)
-direct_url = st.text_input("Direct URL")
+    # Country dropdown options
+    countries = ["Australia", "USA", "Canada", "New Zealand"]
+    # st.write(os.getcwd())
+    # Form inputs
+    country = st.selectbox("Select Country", countries)
+    name = st.text_input("Search Name")
+    city = st.text_input("City")
 
-# Submit button
-if st.button("Collect Data"):
-    # Validation check
-    if country and name and city or direct_url:
-        if direct_url:
-            # st.write(f"Collecting Data for : {name}, {city}, {country}")
-            yp_au_scrape(name, city, direct_url)
-            
+    st.markdown(
+        "<h3 style='text-align:center; color:blue;'>OR</h3>",
+        unsafe_allow_html=True,
+    )
+    direct_url = st.text_input("Direct URL")
+
+    # Submit button
+    if st.button("Collect Data"):
+        # Validation check
+        if country and name and city or direct_url:
+            if not direct_url:
+                st.write(f"Collecting Data for : {name}, {city}, {country}")
+            All_result_dict = yp_au_scrape(name, city, direct_url)
+
+            result_df = pd.DataFrame(All_result_dict.values())
+            json_format = json.dumps(All_result_dict)
+            # print(All_result_dict)
+
+            csv_col, json_col, sqlite_col = st.columns(3)
+
+            if csv_col.download_button(
+                "Download CSV",
+                result_df.to_csv(index=False).encode("utf-8"),
+                file_name="CSV_output.csv",
+            ):
+                st.success("CSV file downloaded successfully!")
+
+            if json_col.download_button(
+                "Download JSON",
+                json_format.encode("utf-8"),
+                file_name="JSON_output.json",
+            ):
+                st.success("JSON file downloaded successfully!")
+
+            # Create a temporary file for SQLite database
+            temp_file_name = "yp_sqlite_output.db"
+            with sqlite3.connect(temp_file_name) as conn:
+                # Replace 'your_table_name' with the desired table name
+                result_df.to_sql("yp_table", conn, index=False, if_exists="replace")
+
+            # Create download button for SQLite
+
+            with open(temp_file_name, "rb") as db_file:
+                db_bytes = db_file.read()
+
+            if sqlite_col.download_button(
+                label="Download SQLite",
+                data=db_bytes,
+                key="download_sqlite",
+                file_name=temp_file_name,
+                mime="application/octet-stream",
+            ):
+                st.success("SQLite database file downloaded successfully!")
+
         else:
-            st.write(f"Collecting Data for : {name}, {city}, {country}")
-            yp_au_scrape(name, city)
-
-    else:
-        st.warning("Please fill in all the inputs before collecting data.")
+            st.warning("Please fill in all the inputs before collecting data.")
--- yp_scraper.py
+++ yp_scraper.py
@@ -11,10 +11,11 @@ import os
 import re
 import pandas as pd
 import sqlite3
-from io import BytesIO
+import math
 
 
 def yp_au_scrape(clue="", loc_clue="", direct_url=""):
+    All_result_dict = {}
     try:
         # clue = "chirofactor"
         # loc_clue = "Brisbane"
@@ -30,121 +31,100 @@ def yp_au_scrape(clue="", loc_clue="", direct_url=""):
         main_soup = Bs(main_resp.text, "html.parser")
         dom = etree.HTML(str(main_soup))
 
-        card_list = main_soup.find_all(
-            "div",
-            class_="Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded",
+        item_count = int(
+            main_soup.find(
+                "h2",
+                class_="MuiTypography-root jss310 MuiTypography-body2 MuiTypography-paragraph",
+            )
+            .text.strip()
+            .split()[0]
         )
-        st.write(len(card_list))
+        cnt = 0
+        max_page = math.ceil(item_count / 35)
+        print(max_page)
+        for page in range(1, max_page):
+            main_resp = requests.get(
+                f"{main_url}&pageNumber={page}", headers={"User-Agent": "Mozilla/5.0"}
+            )
+            print(f"{main_url}&pageNumber={page}")
 
-        All_result_dict = {}
-        for cnt, card in enumerate(card_list):
-            try:
-                card_dom = etree.HTML(str(card))
-                title = "".join(
-                    card_dom.xpath(
-                        "//div[@class='Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded']//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']//h3/text()"
+            main_soup = Bs(main_resp.text, "html.parser")
+            dom = etree.HTML(str(main_soup))
+
+            card_list = main_soup.find_all("div", class_="Box__Div-sc-dws99b-0 dAyAhR")
+            print(len(card_list))
+            for card in card_list:
+                try:
+                    card_dom = etree.HTML(str(card.parent()))
+                    title = "".join(
+                        card_dom.xpath(
+                            "//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']//h3/text()"
+                        )[:1]
                     )
-                )
-                phone_No = "".join(
-                    card_dom.xpath(
-                        "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-text ButtonPhone MuiButton-textPrimary MuiButton-fullWidth']//span/text()"
+
+                    phone_No = "".join(
+                        card_dom.xpath(
+                            "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-text ButtonPhone MuiButton-textPrimary MuiButton-fullWidth']//span/text()"
+                        )[:1]
                     )
-                )
-                website = "".join(
-                    card_dom.xpath(
-                        "//a[@class='MuiButtonBase-root MuiButton-root MuiButton-text ButtonWebsite MuiButton-textSecondary MuiButton-fullWidth']/@href"
+                    website = "".join(card_dom.xpath("//a[.='View Website']/@href")[:1])
+                    yp_url = "https://www.yellowpages.com.au" + "".join(
+                        card_dom.xpath(
+                            "//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']/@href"
+                        )[:1]
                     )
-                )
-                yp_url = "https://www.yellowpages.com.au" + "".join(
-                    card_dom.xpath(
-                        "//div[@class='Box__Div-sc-dws99b-0 iOfhmk MuiPaper-root MuiCard-root PaidListing MuiPaper-elevation1 MuiPaper-rounded']//a[@class='MuiTypography-root MuiLink-root MuiLink-underlineNone MuiTypography-colorPrimary']/@href"
+                    yp_resp = requests.get(
+                        yp_url, headers={"User-Agent": "Mozilla/5.0"}
                     )
-                )
-                yp_resp = requests.get(yp_url, headers={"User-Agent": "Mozilla/5.0"})
-                yp_soup = Bs(yp_resp.text, "html.parser")
-                yp_dom = etree.HTML(str(yp_soup))
-                email = "".join(
-                    yp_dom.xpath(
-                        "//a[@class='contact contact-main contact-email']/@data-email"
+                    yp_soup = Bs(yp_resp.text, "html.parser")
+                    yp_dom = etree.HTML(str(yp_soup))
+                    email = "".join(
+                        yp_dom.xpath(
+                            "//a[@class='contact contact-main contact-email']/@data-email"
+                        )
                     )
-                )
+                    if not email:
+                        Google_Search_url = (
+                            f"https://www.google.com/search?q={title}+email+address"
+                        )
+                        search_resp = requests.get(
+                            Google_Search_url, headers={"User-Agent": "Mozilla/5.0"}
+                        )
+                        email_pattern = (
+                            r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
+                        )
+                        email = ", ".join(re.findall(email_pattern, search_resp.text))
 
-                if not email:
-                    Google_Search_url = (
-                        f"https://www.google.com/search?q={title}+email+address"
+                    address = "".join(
+                        yp_dom.xpath(
+                            "//div[@class='listing-address mappable-address mappable-address-with-poi']/text()"
+                        )
                     )
-                    search_resp = requests.get(
-                        Google_Search_url, headers={"User-Agent": "Mozilla/5.0"}
-                    )
-                    email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
-                    email = ", ".join(re.findall(email_pattern, search_resp.text))
 
-                address = "".join(
-                    yp_dom.xpath(
-                        "//div[@class='listing-address mappable-address mappable-address-with-poi']/text()"
-                    )
-                )
+                    result_dict = {
+                        "YP URL": yp_url,
+                        "Business Name": title,
+                        "Website URL": website,
+                        "Phone Number": phone_No,
+                        "Physical Address": address,
+                        "Email": email,
+                    }
+                    All_result_dict[result_dict["Business Name"]] = result_dict
 
-                result_dict = {
-                    "YP URL": yp_url,
-                    "Business Name": title,
-                    "Website URL": website,
-                    "Phone Number": phone_No,
-                    "Physical Address": address,
-                    "Email": email,
-                }
-                All_result_dict[result_dict["Business Name"]] = result_dict
+                    headers = result_dict.keys()
 
-                headers = result_dict.keys()
+                    df = pd.DataFrame([result_dict.values()], columns=headers)
 
-                df = pd.DataFrame([result_dict.values()], columns=headers)
+                    if cnt == 0:
+                        st_df = st.dataframe(df)
+                    else:
+                        st_df.add_rows(df)
 
-                if cnt == 0:
-                    st_df = st.dataframe(df)
-                else:
-                    st_df.add_rows(df)
-
-            except Exception as e:
-                print(e)
-                pass
-
-        result_df = pd.DataFrame(All_result_dict.values())
-        json_format = json.dumps(All_result_dict)
-        print(All_result_dict)
-
-        csv_col, json_col = st.columns(2)
-
-        if csv_col.download_button(
-            "Download CSV",
-            result_df.to_csv(index=False).encode("utf-8"),
-            file_name="output.csv",
-        ):
-            st.success("CSV file downloaded successfully!")
-
-        if json_col.download_button(
-            "Download JSON",
-            json_format.encode("utf-8"),
-            file_name="output.json",
-        ):
-            st.success("JSON file downloaded successfully!")
-
-        # Create a temporary file for SQLite database
-        # with tempfile.NamedTemporaryFile(suffix=".db", delete=False) as temp_file:
-        #     temp_file_name = temp_file.name
-
-        #     # Convert DataFrame to SQLite database
-        #     with sqlite3.connect(temp_file_name) as conn:
-        #         result_df.to_sql(
-        #             "your_table_name", conn, index=False, if_exists="replace"
-        #         )
-
-        # # Create download button
-        # if st.button("Download SQLite"):
-        #     st.download_button(
-        #         label="Download SQLite", data=temp_file_name, key="download_sqlite"
-        #     )
-        #     st.success("SQLite database file downloaded successfully!")
+                    cnt += 1
+                except Exception as e:
+                    print(e)
 
     except Exception as e:
-        st.write("Can't Find Data")
-        print(yp_url, e)
+        print(e)
+
+    return All_result_dict
